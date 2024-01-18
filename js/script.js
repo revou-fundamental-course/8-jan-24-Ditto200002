17eac7e88b57578cbe1899fab761a615cd05ebab
arukieru <137676740+awanama1@users.noreply.github.com>
2024-01-18 23:36:14 +0700

    test

--- js/script.js
+++ js/script.js
@@ -5,6 +5,21 @@ const reset = document.getElementById("reset");
 const sisi = document.getElementById("sisi");
 const warn = document.getElementById("peringatan");
 
+function hitungLuas() {
+    document.getElementById("rumus-keliling").classList.add("hide");
+    document.getElementById("rumus-luas").classList.remove("hide");
+    document.getElementById("toggleLuas").classList.add("biru");
+    document.getElementById("toggleKeliling").classList.remove("biru");
+}
+
+function hitungKeliling() {
+    document.getElementById("rumus-luas").classList.add("hide");
+    document.getElementById("rumus-keliling").classList.remove("hide");
+    document.getElementById("toggleKeliling").classList.add("biru");
+    document.getElementById("toggleLuas").classList.remove("biru");
+}
+
+
 function valid(sisi) {
     const regex = /\d/;
     return sisi.match(regex);
@@ -30,6 +45,9 @@ function bersih() {
     luas.value = ''
     sisi.value = ''
 }
+document.getElementById("toggleLuas").addEventListener("click", hitungLuas);
+document.getElementById("toggleKeliling").addEventListener("click", hitungKeliling);
+
 
 hasil.addEventListener("click", hitung);
 reset.addEventListener("click", bersih);
